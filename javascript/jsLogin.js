49db9be4de97b6b02361e19b6a70bbb9ecab20b2
lintangqurota <lintangqurota73@gmail.com>
2021-01-15 06:35:27 +0700

    edit

--- javascript/jsLogin.js
+++ javascript/jsLogin.js
@@ -3,16 +3,16 @@ function validasi(){
     var username = document.forms["myForm"]["username"].value;
     var password = document.forms["myForm"]["password"].value;
     if
-    ( username == "user" && password == "12345"){
+    ( username == "user" && password == "1234567"){
       return
       true;
     }
     else if(username =="" || password ==""){
-      alert("Masukkan Username=user dan password=12345 sebagai inputan");
+      alert("Masukkan Username=user dan password=1234567 sebagai inputan");
       return false;
     }
     else{
-      alert("Username dan password SALAH! Masukkan Username=user dan password=12345 sebagai inputan");
+      alert("Username dan password SALAH!");
       return false;
     }
 }
